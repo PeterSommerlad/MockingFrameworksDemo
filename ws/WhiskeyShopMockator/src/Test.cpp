93bf950aa9100bbee157a10611d1d84dcc6e5a7e
Peter Sommerlad <peter.sommerlad@hsr.ch>
2017-08-23 17:02:52 +0200

    before adding mockator setmailer fake

--- ws/WhiskeyShopMockator/src/Test.cpp
+++ ws/WhiskeyShopMockator/src/Test.cpp
@@ -4,15 +4,16 @@
 #include "cute_runner.h"
 #include <map>
 
+#include "mockator.h"
 namespace {
 using namespace std::string_literals;
 
 const auto TALISKER = "Talisker"s;
 }
-
+namespace TDD {
 struct Warehouse {
 	int getInventory(std::string const& s) const {
-		auto it= wh.find(s);
+		auto const it = wh.find(s);
 		return (it != wh.end())?it->second:0;
 	}
 
@@ -32,7 +33,7 @@ struct Warehouse {
 
 /// TDD Warehouse
 void WarehouseHasInventory(){
-	Warehouse warehouse{};
+	Warehouse const warehouse { };
 	ASSERT(not warehouse.hasInventory(TALISKER,50));
 }
 void WarehouseAdd(){
@@ -56,15 +57,28 @@ void WarehouseGetInventory(){
 }
 ///TDD Order
 struct Order {
+
 	void fill(Warehouse& warehouse) {
+		if (warehouse.hasInventory(what, howmany)){
+			warehouse.remove(what,howmany);
+			setFilled();
+		}
 	}
 
 	bool isFilled() const {
-		return bool();
+		return filled;
 	}
 
-	Order(std::string const & whisky, int i) {
+	Order(std::string const & whisky, int i)
+	:what{whisky},howmany{i}{
 	}
+private:
+	void setFilled() {
+		filled=true;
+	}
+	bool filled{};
+std::string const what;
+int const howmany;
 };
 
 void OrderCtor(){
@@ -77,20 +91,161 @@ void OrderFillFromWarehouse(){
 	order.fill(warehouse);
 	ASSERT(not order.isFilled());
 }
+}
+
+
+namespace CT {
+struct Warehouse {
+	int getInventory(std::string const& s) const {
+		auto const it = wh.find(s);
+		return (it != wh.end())?it->second:0;
+	}
+
+	void remove(std::string const & s, int i) {
+		wh[s]-=i;
+	}
+
+	void add(std::string const & s, int i) {
+		wh[s]+=i;
+	}
+
+	bool hasInventory(const std::string& s, int i) const {
+		return getInventory(s)>=i;
+	}
+	std::map<std::string,int> wh{};
+};
+
+/// TDD Warehouse
+void WarehouseHasInventory(){
+	Warehouse const warehouse { };
+	ASSERT(not warehouse.hasInventory(TALISKER,50));
+}
+void WarehouseAdd(){
+	Warehouse warehouse{};
+	warehouse.add(TALISKER,50);
+	ASSERT( warehouse.hasInventory(TALISKER,50));
+}
+
+
+void WarehouseRemove(){
+	Warehouse warehouse{};
+	warehouse.add(TALISKER,50);
+	warehouse.remove(TALISKER,50);
+	ASSERT(not warehouse.hasInventory(TALISKER,50));
+}
+
+void WarehouseGetInventory(){
+	Warehouse warehouse{};
+	warehouse.add(TALISKER,50);
+	ASSERT_EQUAL(50,warehouse.getInventory(TALISKER));
+}
+///TDD Order
+template<typename T1 = Warehouse> struct OrderT {
+	void fill(T1& warehouse) {
+		if (warehouse.hasInventory(what, howmany)) {
+			warehouse.remove(what, howmany);
+			setFilled();
+		}
+	}
+
+	bool isFilled() const {
+		return filled;
+	}
+
+	OrderT(std::string const & whisky, int i) :	what { whisky }, howmany { i } {	}
+
+private:
+	void setFilled() {
+		filled = true;
+	}
+
+	std::string const what;
+	int const howmany;
+	bool filled {};
+};
+using Order = OrderT<>;
+
+void OrderCtor(){
+	Order order(TALISKER,50);
+	ASSERT(not order.isFilled());
+}
 
 
 
+void OrderFillFromWarehouse(){
+	INIT_MOCKATOR();
+	static std::vector<calls> allCalls(1);
+	struct MockWarehouse {
+		size_t const mock_id;
+
+		MockWarehouse()
+		:mock_id(reserveNextCallId(allCalls))
+		{
+			allCalls[mock_id].push_back(call("MockWarehouse()"));
+		}
+
+		bool hasInventory(std::string const & what, int howmany) const {
+			allCalls[mock_id].push_back(call("hasInventory(const std::string&, int) const", what, howmany));
+			return bool();
+		}
+
+		void remove(std::string const & what, int howmany) const {
+			allCalls[mock_id].push_back(call("remove(const std::string&, int) const", what, howmany));
+		}
+	};
+	MockWarehouse warehouse { };
+	OrderT<MockWarehouse> order(TALISKER,50);
+	order.fill(warehouse);
+	ASSERT(not order.isFilled());
+	calls expectedMockWarehouse{
+		call("MockWarehouse()"),
+		call("hasInventory(const std::string&, int) const", TALISKER,50),
+		call("remove(const std::string&, int) const", TALISKER,50)
+	};
+	ASSERT_EQUAL(expectedMockWarehouse, allCalls[1]);
+}
+
+struct MockWarehouse {
+	bool hasInventory(std::string const & what, int howmany) const {
+		return bool();
+	}
+
+	void remove(std::string const & what, int howmany) const {
+	}
+};
+
+void UnfulfilledOrderSendsEmailIfMailerIsSet(){
+	OrderT<MockWarehouse> order(TALISKER,51);
+	MockWarehouse wh{};
+	order.fill(wh);
+	ASSERTM("start writing tests", false);
+}
+
+
+
+}
 
 
 bool runAllTests(int argc, char const *argv[]) {
 	cute::suite s { };
 	//TODO add your test here
+	{ using namespace TDD;
 	s.push_back(CUTE(WarehouseHasInventory));
 	s.push_back(CUTE(WarehouseRemove));
 	s.push_back(CUTE(WarehouseAdd));
 	s.push_back(CUTE(WarehouseGetInventory));
 	s.push_back(CUTE(OrderCtor));
 	s.push_back(CUTE(OrderFillFromWarehouse));
+	}
+	{ using namespace CT;
+	s.push_back(CUTE(WarehouseHasInventory));
+	s.push_back(CUTE(WarehouseRemove));
+	s.push_back(CUTE(WarehouseAdd));
+	s.push_back(CUTE(WarehouseGetInventory));
+	s.push_back(CUTE(OrderCtor));
+	s.push_back(CUTE(OrderFillFromWarehouse));
+	s.push_back(CUTE(UnfulfilledOrderSendsEmailIfMailerIsSet));
+	}
 	cute::xml_file_opener xmlfile(argc, argv);
 	cute::xml_listener<cute::ide_listener<>> lis(xmlfile.out);
 	auto runner = cute::makeRunner(lis, argc, argv);
